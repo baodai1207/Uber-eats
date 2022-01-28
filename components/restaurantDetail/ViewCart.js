5c560dfca0283d56f22355a9255b815780c6b6cc
baodai1207 <dai.huynh@ucdenver.edu>
2022-01-28 11:48:01 -0700

    added screen loading proccessing orders

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, Modal, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
-import app from "../../firebase";
 import OrderItem from "./OrderItem";
 import firebase from "../../firebase";
+import LottieView from "lottie-react-native";
 
 export default function ViewCart({ navigation }) {
 	const [modalVisible, setModalVisible] = useState(false);
+	const [loading, setLoading] = useState(false);
 
 	const { items, restaurantName } = useSelector(
 		state => state.cartReducer.selectedItems
@@ -23,14 +24,20 @@ export default function ViewCart({ navigation }) {
 	// });
 
 	const addOrderToFirebase = () => {
+		setLoading(true);
 		const db = firebase.firestore();
-		db.collection("orders").add({
-			items: items,
-			restaurantName: restaurantName,
-			createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-		});
-		setModalVisible(false);
-		navigation.navigate("OrderCompleted");
+		db.collection("orders")
+			.add({
+				items: items,
+				restaurantName: restaurantName,
+				createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+			})
+			.then(() => {
+				setTimeout(() => {
+					setLoading(false);
+					navigation.navigate("OrderCompleted");
+				}, 2500);
+			});
 	};
 
 	const styles = StyleSheet.create({
@@ -91,6 +98,7 @@ export default function ViewCart({ navigation }) {
 								}}
 								onPress={() => {
 									addOrderToFirebase();
+									setModalVisible(false);
 								}}
 							>
 								<Text style={{ color: "white", fontSize: 20 }}>Checkout</Text>
@@ -165,6 +173,28 @@ export default function ViewCart({ navigation }) {
 							<Text style={{ color: "white", fontSize: 20 }}>{totalUSD}</Text>
 						</TouchableOpacity>
 					</View>
+				</View>
+			) : (
+				<></>
+			)}
+			{loading ? (
+				<View
+					style={{
+						backgroundColor: "black",
+						position: "absolute",
+						opacity: 0.6,
+						justifyContent: "center",
+						alignItems: "center",
+						height: "100%",
+						width: "100%",
+					}}
+				>
+					<LottieView
+						style={{ height: 200 }}
+						source={require("../../assets/animations/scanner.json")}
+						autoPlay
+						speed={3}
+					/>
 				</View>
 			) : (
 				<></>
